4f3ed61137f4f67e54a138c94e6869ebdfe76bcd
Ajay Singh <ajayk40@gmail.com>
2016-08-25 21:49:05 +0530

    added bands

--- mmb/mmb/urls.py
+++ mmb/mmb/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^data/', include('mdata.urls')),
     url(r'^users/', include('users.urls')),
+    url(r'^bands/', include('bands.urls')),
     url(r'^muse/', include('muse.urls')),
     url(r'^auth/', include('rest_framework_social_oauth2.urls'))
 ]
