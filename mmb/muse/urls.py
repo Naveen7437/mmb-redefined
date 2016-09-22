1b35db395e7f4d8361b4d04a3c378415fc330abe
Naveen7437 <naveenkh7437@gmail.com>
2016-09-23 00:53:47 +0530

    adding song url (api)

--- mmb/muse/urls.py
+++ mmb/muse/urls.py
@@ -2,13 +2,12 @@ from django.conf.urls import patterns, url, include
 from rest_framework import routers
 router = routers.SimpleRouter()
 
-from muse.views import SongViewset
-
-
+from muse.views import SongViewset, SongLikeViewset
 
 router = routers.DefaultRouter()
-router.register(r'song', SongViewset)
+router.register(r'^song', SongViewset)
+router.register(r'song-like', SongLikeViewset)
 
 urlpatterns = [
-    url(r'^', include(router.urls)),
+    url(r'', include(router.urls)),
 ]
